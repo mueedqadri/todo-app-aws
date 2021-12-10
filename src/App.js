50549d104a54d810f26ceca999eeaf16ae36b8a1
Abdul Mueed Qadri <mueedq12@gmail.com>
2021-12-10 17:51:08 -0400

    update

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import ToDo from "./components/Todo";
 import FeedBack from "./components/FeedBack";
 import SecuredRoute from "./components/SecuredRoute";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import React, { Fragment } from "react";
 import Header from "./components/Header";
 
 function App() {
   return (
-    
     <Router>
       <Fragment>
         <div className="App">
           <Header></Header>
           <Routes>
+          <Route path="/" element={<Login />} />
+
             <Route path="/" element={<SecuredRoute />}>
               <Route path="/list" element={<ToDo />} />
             </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/feedback" element={<FeedBack />} />
-            <Route path="/" element={<SignUp />} />
           </Routes>
         </div>
       </Fragment>
